1cff0fc78a5d825ae78eab6bfd6c9d5444a6bb98
Micah Carroll <micahgester@gmail.com>
2020-01-30 08:00:51 -0500

    first inital push

--- base.js
+++ base.js
@@ -2,7 +2,7 @@ document.addEventListener("mousemove" , function (event) {
     const x = event.pageX
     const y = event.pageY
 
-    document.querySelectorAll ("div").forEach(div => {  
+    document.querySelectorAll (".text-section > div").forEach(div => {  
         const dx = div.offsetLeft + 50 - x
         const dy = div.offsetTop + 50 - y
         const dist = Math.sqrt(dx * dx + dy * dy)
